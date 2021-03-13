dab8c5a785c63a33e455110b9c5a3fb0e3f8d6b7
Recxhan <75570730+Recxhan@users.noreply.github.com>
2021-03-13 18:33:37 +0800

    FSJ_Project4

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FSJ_Project4
  OOP Game Show App
+I have made some app style changes as below:
+
+1. If the player guesses the correct letter, the matched letter showing at the top would grow a bit bigger.
+
+2. If the player guesses the wrong letter, the letter in the qwerty would turn white as well as the background color so as to make it disappear. 
--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Project 4 - OOP Game App
  * app.js */
 
+// Varibales
 const phraseUl = document.querySelector('#phrase ul');
 const startBtn = document.querySelector('#btn__reset');
 const overlay = document.querySelector('#overlay');
@@ -10,13 +11,24 @@ const heading = overlay.querySelector('#game-over-message');
 const heartImage = document.querySelectorAll('.tries img')
 let game;
 
+// Game Start
 startBtn.addEventListener('click', () => {
     game = new Game();
     game.startGame();
 })
 
+// qwerty(mouth click)
 qwerty.forEach(letter => {
     letter.addEventListener('click', (e) => {
         game.handleInteraction(e.target);
     })
+})
+
+// qwerty(keyboard)
+document.addEventListener('keyup', (e) => {
+    for(let i = 0; i < qwerty.length; i++){
+        if(qwerty[i].textContent === e.key.toLowerCase() && !qwerty[i].disabled){
+            game.handleInteraction(qwerty[i])
+        }
+    }
 })
